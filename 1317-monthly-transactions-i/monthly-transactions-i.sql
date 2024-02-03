998a25faaac0ab78138ec4f3f098bd0d397cc36d
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2024-02-03 15:48:04 -0800

    Time: 2084 ms (85.14%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
-SELECT SUBSTRING(format(trans_date,'yyyy-MM-dd hh:mm:ss tt'),1,7)  as month, 
+SELECT LEFT(t.trans_date, '7') month, 
 t.country country,
 count(*) as trans_count,
 SUM(CASE WHEN t.state = 'approved' THEN 1 ELSE 0 END) as approved_count,
@@ -7,4 +7,4 @@ SUM(CASE WHEN t.state = 'approved' THEN 1 ELSE 0 END) as approved_count,
 SUM(CASE WHEN t.state = 'approved' THEN t.amount ELSE 0 END) as approved_total_amount
 
 FROM Transactions as t
-GROUP BY SUBSTRING(format(trans_date,'yyyy-MM-dd hh:mm:ss tt'),1,7) ,country
+GROUP BY LEFT(t.trans_date, '7') ,country
