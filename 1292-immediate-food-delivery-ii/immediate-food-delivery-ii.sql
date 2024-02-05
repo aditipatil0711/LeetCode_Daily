81f964c2c0810546286983593e23c19357a11336
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2024-02-04 19:07:32 -0800

    Time: 1693 ms (89.36%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,21 +1,24 @@
 /* Write your T-SQL query statement below */
-
-
-WITH First_Order AS (
-SELECT customer_id, preference
-FROM (
-select 
-customer_id
-,order_date
-,customer_pref_delivery_date
-,row_number() over (partition by customer_id order by order_date) as rn 
-, case when order_date = customer_pref_delivery_date then 'immediate' else 'scheduled' end as preference
-from delivery 
-) as SubQ 
-where SubQ.rn = 1
+with first_order as (
+    select 
+        customer_id,
+        order_date,
+        customer_pref_delivery_date,
+        rnk 
+    from (select 
+        customer_id,
+        order_date,
+        customer_pref_delivery_date,
+        rank() over (partition by customer_id order by order_date) as rnk 
+    from Delivery) a
+    where rnk = 1
 )
-
-SELECT 
-ROUND(SUM(CASE WHEN preference = 'immediate' then 1 else 0 end + 0.00 )*100 / COUNT(*), 2) as immediate_percentage
-
+select 
+round(
+    sum(
+        case
+	    when order_date = customer_pref_delivery_date then 1
+	    else 0
+    end)*100.0/count(customer_id)
+,2) as immediate_percentage
 from first_order
