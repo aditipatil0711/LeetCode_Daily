701551f06d7a8d06fc2ac81f0863b4d8efc24432
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2024-02-22 22:19:43 -0800

    Time: 542 ms (81.69%) | Memory: 16.9 MB (64.10%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,13 +1,18 @@
 class Solution(object):
     def findJudge(self, n, trust):
-        trustcount = [0] * (n + 1) 
+        
+        if n == 1:
+            return 1 if not trust else -1
 
-        for relation in trust:
-            trustcount[relation[0]] -= 1
-            trustcount[relation[1]] += 1
+        trustCounts = [0] * (n + 1)
+        trustedByCounts = [0] * (n + 1)
+
+        for a, b in trust:
+            trustCounts[a] += 1
+            trustedByCounts[b] += 1
 
         for i in range(1, n + 1):
-            if trustcount[i] == n - 1:
+            if trustCounts[i] == 0 and trustedByCounts[i] == n - 1:
                 return i
 
-        return -1  
+        return -1
