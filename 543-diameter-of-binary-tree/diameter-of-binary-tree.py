20abd28f3ea6b18e242c33a74620b41c83633487
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2024-02-27 02:04:08 -0800

    Time: 14 ms (98.51%) | Memory: 14.4 MB (88.64%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -11,15 +11,20 @@ class Solution(object):
         :rtype: int
         """
         
-        res = [0]
+        global res
+        res = 0
 
         def dfs(root):
-            if not root:
-                return -1
-            left = dfs(root.left)
-            right = dfs(root.right)
+            global res
 
-            res[0] = max(res[0],2+left+right)
-            return 1+ max(left,right)
+            if not root: return 0
+
+            l = dfs(root.left)
+            r = dfs(root.right)
+
+            res= max(res, l + r) 
+
+            return 1 + max(l,r)
+            
         dfs(root)
-        return res[0]
+        return res
