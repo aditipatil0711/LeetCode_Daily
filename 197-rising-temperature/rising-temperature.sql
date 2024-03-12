a852c5ac854e3e8a621bab2988a8525bfe2a36a5
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2024-03-11 23:54:11 -0700

    Time: 1110 ms (49.06%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,5 @@
+/* Write your T-SQL query statement below */
+
+SELECT w1.id as Id from Weather as w1 , Weather as w2
+WHERE w1.temperature > w2.temperature
+AND  DATEDIFF(DAY,w2.recordDate,w1.recordDate) = 1;
