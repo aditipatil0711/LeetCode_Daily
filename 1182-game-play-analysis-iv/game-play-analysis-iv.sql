a316bbc06d768b3b6fe5a66d1db591020905de3e
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2024-03-16 01:11:58 -0700

    Time: 4454 ms (10.58%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,15 @@
+/* Write your T-SQL query statement below 
+1. Join with same table
+2. group by number of people
+3.
+*/
+
+WITH CTE AS
+(
+    SELECT player_id,datediff(day,min(event_date) OVER(PARTITION BY player_id), event_date)
+    AS daydiff from activity
+
+)
+
+SELECT round(COUNT(CASE WHEN daydiff = 1 THEN player_id END) * 1.0 / COUNT(distinct player_id) ,2) as fraction
+FROM cte;
