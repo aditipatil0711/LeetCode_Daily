73fd7eaeacce46e7fa97118f4c0b636eca671565
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2024-03-31 18:30:31 -0700

    Time: 9 ms (86.06%) | Memory: 11.8 MB (26.33%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -9,9 +9,11 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        slow,fast = head, head
+        slow_pointer = head
+        fast_pointer = head
 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        return slow
+        while fast_pointer is not None and fast_pointer.next is not None:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+
+        return slow_pointer
