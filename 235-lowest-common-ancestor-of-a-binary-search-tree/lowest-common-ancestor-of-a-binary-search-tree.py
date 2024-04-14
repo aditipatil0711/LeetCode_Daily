c15b24f28ff2386d29a80cf458dbed213a440721
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2024-04-14 04:41:24 -0700

    Time: 37 ms (96.71%) | Memory: 19.8 MB (22.05%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -13,20 +13,11 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        parent_val = root.val
-
-        # Value of p
-        p_val = p.val
-
-        # Value of q
-        q_val = q.val
-
-        # If both p and q are greater than parent
-        if p_val > parent_val and q_val > parent_val:    
-            return self.lowestCommonAncestor(root.right, p, q)
-        # If both p and q are lesser than parent
-        elif p_val < parent_val and q_val < parent_val:    
-            return self.lowestCommonAncestor(root.left, p, q)
-        # We have found the split point, i.e. the LCA node.
-        else:
-            return root
+        cur =root
+        while cur:
+            if p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+            elif p.val < cur.val and q.val < cur.val:
+                cur = cur.left
+            else:
+                return cur
