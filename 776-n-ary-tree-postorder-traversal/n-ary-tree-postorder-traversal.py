de7438504313ad02523e1599e708713432676b36
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2024-08-26 11:40:15 -0700

    Time: 48 ms (38.87%) | Memory: 18.1 MB (95.62%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -8,15 +8,17 @@ class Node:
 
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
-        result = []
-        
-        def traverse(node):
-            if not node:
-                return
-            for child in node.children:
-                traverse(child)
-            result.append(node.val)
-        
-        traverse(root)
-        return result
-        
+        res = []
+        if not root:
+            return []
+        stack = [(root, False)]
+
+        while stack:
+            node, visited = stack.pop()
+            if visited:
+                res.append(node.val)
+            else:
+                stack.append((node, True))
+                for c in node.children[::-1]:
+                    stack.append((c, False))
+        return res
