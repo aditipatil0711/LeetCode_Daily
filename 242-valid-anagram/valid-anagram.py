804c17da6683c4ab772e86e53d57b83b96697774
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2024-08-27 02:17:45 -0700

    Time: 51 ms (46.58%) | Memory: 17.8 MB (5.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,12 +1,5 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        s1 = list(sorted(s))
-        s2 = list(sorted(t))
-        return s1==s2
-       
-        
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        l1 = list(sorted(s))
+        l2 = list(sorted(t))
+        return l1 == l2
