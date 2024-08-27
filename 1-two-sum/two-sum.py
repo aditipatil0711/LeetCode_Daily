4e65103503d860ce5d2dfd2ed50659f9f6c87c7d
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2024-08-27 02:21:51 -0700

    Time: 53 ms (87.40%) | Memory: 17.7 MB (29.80%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,23 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         dict1 = {}
         for i in range(len(nums)):
-            c = target-nums[i]
+            c =  target-nums[i]
             if c in dict1:
                 return [i,dict1[c]]
             dict1[nums[i]] = i
-            
-        
-            
-            
-
-
-        
-
-
         
