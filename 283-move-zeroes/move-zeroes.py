1486abe9d2c1304ebf30373e837d46cb32b85a92
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2025-01-21 15:15:32 -0800

    Time: 10 ms (42.02%) | Memory: 13.5 MB (58.97%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,11 +5,14 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         l = 0
+        i=0
         for i,v in enumerate(nums):
             if v!=0:
-                nums[l],nums[i] = nums[i],nums[l]
+                nums[l],nums[i]=nums[i],nums[l]
                 l+=1
-        
-        return nums
+
+        return nums        
+            
+
 
         
