149146c10c55d2c95accde897771175e9927219f
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2025-02-06 16:59:49 -0800

    Time: 4 ms (37.20%) | Memory: 15 MB (83.49%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -10,11 +10,19 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        if root is None:
-            return 0
-        else:
-            leftH = self.maxDepth(root.left)
-            rightH = self.maxDepth(root.right)
-            return max(leftH,rightH)+1
+        stack = []
+        if root is not None:
+            stack.append((1,root))
+
+        depth = 0
+        while stack!=[]:
+            curr,root = stack.pop()
+            if root is not None:
+                depth = max(depth, curr)
+                stack.append((curr + 1, root.left))
+                stack.append((curr + 1, root.right))
+
+        return depth
+
 
         
