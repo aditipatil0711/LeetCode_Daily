170e0f999d41b66539384055f8ac22f646f54a81
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2025-05-01 21:34:38 -0700

    Time: 2 ms (97.37%) | Memory: 41.7 MB (66.15%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,30 +1,27 @@
 class Solution {
-    private HashMap<Character,Character> mappings;
-     
-     public Solution(){
-     this.mappings = new HashMap<Character,Character>();
-        this.mappings.put(')','(');
-        this.mappings.put(']','[');
-        this.mappings.put('}','{');
-     }
     public boolean isValid(String s) {
-       Stack<Character> stack = new Stack<Character>();
-
-       for(int i=0;i<s.length(); i++){
-        char c = s.charAt(i);
-        if(this.mappings.containsKey(c)){
-            char top = stack.empty() ? '#' : stack.pop();
-        
-            if (top != this.mappings.get(c)){
-                return false;
-            }}
-            else{
-                stack.push(c);
+        Stack<Character> stack = new Stack<>();
+        for(char ch: s.toCharArray()){
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
             }
+            else if( ch == ')' && !stack.isEmpty() && stack.peek() == '('){
+                stack.pop();
+            }
+            else if( ch == ']' && !stack.isEmpty() && stack.peek() == '['){
+                stack.pop();
+            }
+            else if( ch == '}' && !stack.isEmpty() && stack.peek() == '{'){
+                stack.pop();
+            }
+
+            else{
+                return false;
+            }
+
         }
-       
+
         return stack.isEmpty();
         
-
     }
 }
