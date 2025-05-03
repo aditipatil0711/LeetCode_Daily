c51ff0d3f562fedb0abf5a40a4a369e16419020b
Aditi Patil <67693038+aditipatil0711@users.noreply.github.com>
2025-05-03 16:45:10 -0700

    Time: 9 ms (22.07%) | Memory: 44.7 MB (37.62%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length()+1];
+
+        dp[0] = true;
+        
+        for(int i = 0; i<=s.length(); i++){
+            for(int j = 0;j<i;j++){
+                String word = s.substring(j,i);
+                if (dp[j] && wordSet.contains(word)){
+                    dp[i] = true;
+                    break;
+                }
+
+            }
+        }
+
+        return dp[s.length()];
+        
+    }
+}
